b6214415422819223e1930b2dc3f92d95cc80574
paragon279 <ishihara.yuudai727@gmail.com>
2024-10-21 05:18:35 -0400

    second commit_header

--- src/header/index.js
+++ src/header/index.js
@@ -15,7 +15,7 @@ const Headermain = () => {
 
   return (
     <>
-      <header className="fixed-top site__header" style={{background: 'white'}}>
+      <header className="fixed-top site__header" >
         <div className="d-flex align-items-center justify-content-between">
           <Link className="navbar-brand nav_ac" to="/">
             {logotext}
