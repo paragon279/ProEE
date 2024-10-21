cb128c3c666e938223eec448d488e15bab16de3e
paragon279 <ishihara.yuudai727@gmail.com>
2024-10-21 04:19:53 -0400

    First push

---
+++ src/content_option.js
@@ -0,0 +1,204 @@
+const logotext = "David, Professional Engineer";
+const meta = {
+    title: "David M",
+    description: "I’m David M, licensed Professional Engineer based in United States",
+};
+
+const introdata = {
+    title: "I’m David M.",
+    animated: {
+        first: "Electrical & Electronic Engineer",
+        second: "PCB Design & Firmware& IoT",
+    },
+    description: "I am an experienced Electrical & Electronics & IoT Engineer with experience providing full-cycle Electrical Engineering services including Hardware, Firmware, Product Design, Prototyping, and Manufacturing.",
+    your_img_url: require('../src/assets/images/david_m.jpg'),
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "I’m David M, licensed Professional Engineer based in United States.I have over 15 years of experience in Electrical & Electronics & IoT Engineering.\n \n  I have practical experience in medical, audio, automotive, Robotics, and industrial products including IoT projects, wearable devices, Smart-home systems, Noise Cancelling Earbuds and Headphones, ECG, EMG, PPG, SpO2 sensors, Blood pressure monitoring, Respiration rate monitoring, automotive control equipment, climate equipment, Radar, Lidar, RF design, Antenna design, hardware equipment for autonomous driving systems, High current and high power electronics (battery chargers, battery balancers, BLDC drivers, frequency converters, inverters, power banks, home power storage systems), High voltage power supplies (up to 30 kV), energy harvesting (solar, piezo, thermal), Solar trackers, Limbs position monitoring, and radio transmitter (2.4GHz, 5GHz) ",
+};
+const worktimeline = [{
+        jobtitle: "Hardware Engineer",
+        where: "Dedicated Drones",
+        date: "2006-2010",
+    },
+    {
+        jobtitle: "Electrical Design Engineer",
+        where: "EscapeSF LLC",
+        date: "2010-2013",
+    },
+    {
+        jobtitle: "Electrical Engineer",
+        where: "A Squared Engineers",
+        date: "2014-2019",
+    },
+    {
+        jobtitle: "Senior Electrical Engineer",
+        where: "Meraqi Medical",
+        date: "2019-2021",
+    },
+    {
+        jobtitle: "Electrical Engineer/Manager",
+        where: "QuestPlay",
+        date: "2021-2024",
+    },
+];
+
+const skills = [
+    {
+        name: "Electrical Engineering",
+        value: 95,
+    },
+    {
+        name: "Electronic Engineering",
+        value: 90,
+    },
+    {
+        name: "C/C++",
+        value: 95,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Embeddd C",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 80,
+    },
+    {
+        name: "AutoCAD",
+        value: 90,
+    },
+    {
+        name: "AltiumDesigner",
+        value: 90,
+    },
+    {
+        name: "Proteus",
+        value: 90,
+    },
+    {
+        name: "PLC",
+        value: 85,
+    },
+    {
+        name: "PCB design",
+        value: 98,
+    },
+    {
+        name: "IoT",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "Hardware",
+        description: "- PCB design (Altium Designer, KiCad, Eagle), Multi-layer PCB layout (6-12 layers), flex-rigid PCB, HDI routing, dense routing, thermal management\n- PCB Prototype manufacturing, debugging, testing, and validation\n- Analog, Digital Signal Processing, Audio signal processing, High-End Audio amplifiers\n- Gigabit Ethernet and USB-C\n- Power electronics, High current PCB routing up to 100A, Power Supplies\n- High-speed routing with length matching, impedance calculations\n- Component selection, BOM optimization, and preparation of manufacturing documentation",
+    },
+    {
+        title: "Firmware",
+        description: "- C, C++, Embedded C, Python, Java, JavaScript\n- STM32, ESP32, ESP8266, nRF51, nRF52, TI, NXP, MSP430, \nNuvoton, Atmel, Beaglebone, PIC, Arduino, Raspberry Pi\n- Compilers & IDE ( GCC, Keil uVision, IAR, Simplicity Studio, Eclipse, CodeWarrior)\n- Robotics\n- AC, DC, BLDC, Step motors\n- Cellular network, GPS/GPRS, GSM\n- Wireless (Bluetooth, BLE, Zigbee, WiFi, Thread, LoRa, LoRaWAN)\n- Wired (I2C, SPI, RS232/RS485, Modbus, CAN)\n- Embedded Linux, Zephyr OS, FreeRTOS, Amazon FreeRTOS, Micrium OS, eCOS",
+    },
+    {
+        title: "IoT",
+        description: "- Cloud Platforms Integration (Firebase, AWS IoT, Ayla, Evrythng)",
+    },
+];
+
+const dataportfolio = [{
+        img: require("../src/assets/images/portfolio/1.jpg"),
+        description: "Lungs Health Analyzer",
+        link: "https://ajprotech.com/selected-work/health-wellness/lungs-health-analyzer.html",
+    },
+    {
+        img: require("../src/assets/images/portfolio/2.jpg"),
+        description: "Handheld device for a tabletop game",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/3.jpg"),
+        description: "Mobile App controls the device via BLE",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/4.jpg"),
+        description: "nRF52 + SIMCOM connect to AWS(MQTTs)",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/5.jpg"),
+        description: "Human Body Temperature Monitor",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/6.jpg"),
+        description: "CAN Bus IOT Hub",
+        link: "#",
+    },
+
+    {
+        img: require("../src/assets/images/portfolio/7.jpg"),
+        description: "PCB Design for Split Mechanical Computer Keyboard",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/8.jpg"),
+        description: "Temperature Control With ESP32",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/9.jpg"),
+        description: "45W USB Port C and USB A Port.",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/10.jpg"),
+        description: "ESP32 LED driver and Audio driver",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/11.jpg"),
+        description: "keyboard with USB-C connector and 14 LED RGB",
+        link: "#",
+    },
+    {
+        img: require("../src/assets/images/portfolio/12.jpg"),
+        description: "USB-C to HDMI Adapter",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "econsultant664@gmail.com",
+    YOUR_FONE: "(763)286-6209",
+    description: "Please feel free to contact me if you have any issues. I'll do my best to help you. Also, if you have any good ideas, don’t hesitate to reach out. I’ll work to make your ideas a reality.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_y9ok36w",
+    YOUR_TEMPLATE_ID: "template_14tfaqg",
+    YOUR_USER_ID: "zhMWs94qwBAAPEDY0",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/header/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./style.css";
+import { VscGrabber, VscClose } from "react-icons/vsc";
+import { Link } from "react-router-dom";
+import { logotext, socialprofils } from "../content_option";
+import Themetoggle from "../components/themetoggle";
+
+const Headermain = () => {
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+    document.body.classList.toggle("ovhidden");
+  };
+
+  return (
+    <>
+      <header className="fixed-top site__header" style={{background: 'white'}}>
+        <div className="d-flex align-items-center justify-content-between">
+          <Link className="navbar-brand nav_ac" to="/">
+            {logotext}
+          </Link>
+          <div className="d-flex align-items-center">
+            <Themetoggle />
+            <button className="menu__button  nav_ac" onClick={handleToggle}>
+              {!isActive ? <VscClose /> : <VscGrabber />}
+            </button>
+
+          </div>
+        </div>
+
+        <div className={`site__navigation ${!isActive ? "menu__opend" : ""}`}>
+          <div className="bg__menu h-100">
+            <div className="menu__wrapper">
+              <div className="menu__container p-3">
+                <ul className="the_menu">
+                  <li className="menu_item ">
+                    <Link onClick={handleToggle} to="/" className="my-3">Home</Link>
+                  </li>
+                  <li className="menu_item">
+                    <Link onClick={handleToggle} to="/portfolio" className="my-3"> Portfolio</Link>
+                  </li>
+                  <li className="menu_item">
+                    <Link onClick={handleToggle} to="/about" className="my-3">About</Link>
+                  </li>
+                  <li className="menu_item">
+                    <Link onClick={handleToggle} to="/contact" className="my-3"> Contact</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
+            <div className="d-flex">
+              {/* <a href={socialprofils.facebook}>Facebook</a>
+            <a href={socialprofils.github}>Github</a>
+            <a href={socialprofils.twitter}>Twitter</a> */}
+              <p className="copyright m-0">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="mr-2" width="16" height="16" style={{ marginRight: '8px' }} >
+                  <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z" />
+                </svg>
+                econsultant664@gmail.com
+                </p>
+            </div>
+            <p className="copyright m-0">copyright __ {logotext}</p>
+          </div>
+        </div>
+      </header>
+      <div className="br-top"></div>
+      <div className="br-bottom"></div>
+      <div className="br-left"></div>
+      <div className="br-right"></div>
+
+    </>
+  );
+};
+
+export default Headermain;
